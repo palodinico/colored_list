6a2b941c69633905f9864a21a48e45a3b4a87f8d
Toshihiro Murata <eb68276@jp.ibm.com>
2015-01-21 14:45:35 +0900

    Add Top page and Sign up page

---
+++ common/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type", content="text/html; charset=utf-8" />
+  <title>Colored Lists | <?php echo $page_title; ?></title>
+  <link rel="stylesheet" href="style.css" type="text/css" />
+  <link rel="shortcut icon" type="image/x-icon" href="http://cdn.css-tricks.com/favicon.ico" />
+  <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js?ver=1.3.2'></script>
+</head>
+
+<body>
+  <div id="page-wrap">
+    <div id="header">
+      <h1><a href="/list/">Colored Lists</a></h1>
+      <div id="control">
+        <!-- IF LOGGED IN -->
+        <p><a href="/list/logout.php" class="button">Log out</a> <a href="/list/accout.php" class="button">Your Account</a></p>
+        <!-- IF LOGGED OUT -->
+        <p><a href="/list/signup.php" class="button">Sign up</a> <a href="/list/login.php">Log in</a></p>
+        <!-- END OF IF STATEMENT -->
+      </div>
--- inc/class.users.inc.php
+++ inc/class.users.inc.php
@@ -8,7 +8,31 @@
  */
 class ColoredListsUsers
 {
-  
+  /**
+   * The database object
+   *
+   * @var object
+   */
+  private $_db;
+
+  /**
+   * Checks for a database object and creates one if none if found
+   *
+   * @param object $db
+   * @return void
+   */
+  public function __construct($db=NULL)
+  {
+  	if(is_object($db))
+  	{
+  	  $this->_db = $db;
+  	}
+  	else
+  	{
+  	  $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
+  	  $this->_db = new POD($dsn, DB_USER, DB_PASS);
+  	}
+  }
 }
 
 ?>
